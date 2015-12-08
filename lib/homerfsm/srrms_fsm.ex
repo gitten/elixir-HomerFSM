0661ccc3df2d09275be277ef1eb585962820f76d
gitten <gitten@users.noreply.github.com>
2015-12-08 01:14:01 -1000

    some wonkies

--- lib/homerfsm/srrms_fsm.ex
+++ lib/homerfsm/srrms_fsm.ex
@@ -1,23 +1,43 @@
 defmodule HomerFSM.SrrmsFsm do
   use Fsm, initial_state: :user_connecting
 
+  @moduledoc"""
+  hrrrrrm
+  """
+
+
   defstate user_connecting do
     defevent welcome_user do
+      # some shnazzy loading/connecting anime here
+      IO.puts("Welcome, (something not funny).")
+      HomerFSM.Reactor.start_link(:core)
+
       next_state(:awaiting_key_press)
     end
   end
 
+
   defstate awaiting_key_press do
     defevent any_key do
+      IO.gets(
+      """
+      Press any key, then `Return`.
+
+     ...or just press `Return`.
+     """)
+      # needs return after any key, so not really any key =(
+
       next_state(:monitoring_core)
     end
   end
 
+
   defstate monitoring_core do
     defevent display_core_temp do
-      IO.puts("Core Temp:fine")
+      # spawn some state face
     end
   end
+
 
   defstate awaiting_decision do
     defevent prompt do
@@ -25,14 +45,16 @@ defmodule HomerFSM.SrrmsFsm do
     end
   end
 
+
   defstate venting_gas do
-    defevent events do
+    defevent some_events_here do
       IO.puts
     end
   end
 
+
   defstate system_lock do
-    defevent do
+    defevent lock do
       IO.puts "manual restart required!?"
     end
   end
